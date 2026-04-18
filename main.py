d5ef0f0856df67f0054220346e2bcd88560a5cd7
samuel2005wq <samuel.acuna-g@mail.escuelaing.edu.co>
2026-04-17 21:50:41 -0500

    Codigo Main para la Camara

--- main.py
+++ main.py
@@ -1,42 +1,26 @@
+from maix import camera, display, image, nn, app, uart
+import os
 
-from maix import camera, display, image, nn, app, comm
-import struct, os
-
-APP_CMD_CLASSIFY_RES = 0x02
-
-def encode_objs(res):
-    '''
-        encode objs info to bytes body for protocol
-        2B max idx, 4B prob, 2B second idx, 4B prob, 2B third idx, 4B prob
-    '''
-    body = b''
-    for obj in res:
-        body += struct.pack("<Hf", obj[0], obj[1])
-    return body
-
-model_path = "model_264492.mud"
-if not os.path.exists(model_path):
-    model_path = "/root/models/maixhub/264492/model_264492.mud"
+model_path = "/root/models/model-264135.maixcam/model_264135.mud"
 classifier = nn.Classifier(model=model_path)
-
 cam = camera.Camera(classifier.input_width(), classifier.input_height(), classifier.input_format())
 disp = display.Display()
 
-p = comm.CommProtocol(buff_size = 1024)
+devices = uart.list_devices()
+print("Puertos disponibles:", devices)
+serial = uart.UART(devices[0], 9600)
 
 while not app.need_exit():
-    # msg = p.get_msg()
-
     img = cam.read()
     res = classifier.classify(img)
-
-    body = encode_objs(res)
-    p.report(APP_CMD_CLASSIFY_RES, body)
-
     max_idx, max_prob = res[0]
-    msg = f"{max_prob:5.2f}: {classifier.labels[max_idx]}"
+    label = classifier.labels[max_idx]
+    msg = f"{max_prob:5.2f}: {label}"
     img.draw_string(10, 10, msg, image.COLOR_RED)
-
-    img = img.resize(disp.width(), disp.height(), image.Fit.FIT_CONTAIN)
     disp.show(img)
+
+    if max_prob > 0.85:
+        serial.write_str(label + "\n")
+
+
 
